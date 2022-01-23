7337e41265c9e3d83bb6336d346e3bf55fb25dff
twig <twigwhite@gmail.com>
2022-01-23 14:56:30 -0800

    done

--- GGJ_Project/Assets/Scripts/MainMenu.cs
+++ GGJ_Project/Assets/Scripts/MainMenu.cs
@@ -24,8 +24,9 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
 	{
+		#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-
+#endif
         Application.Quit();
 	}
 }
